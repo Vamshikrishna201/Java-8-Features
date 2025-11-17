0c50b81a757c0179fb168ac7ba3f8a40cb27c32a
Vamshikrishna <rvkrishna21@gmail.com>
2025-11-17 22:05:16 +0530

    BiPredicate Code Example

---
+++ src/v3_Functional_Interfaces/e5_Predicate.java
@@ -0,0 +1,21 @@
+package v3_Functional_Interfaces;
+
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+
+//BiPredicate Example
+public class e5_Predicate {
+    public static void main(String[] args) {
+        Predicate<Integer> p = i -> i > 10;
+        System.out.println(p.test(4));
+        System.out.println(p.test(14));
+
+        System.out.println("-=-=-=-=-=-=-=-=-=");
+
+        BiPredicate<Integer, Integer> bip = (i, j) -> (i + j) >= 20;
+        System.out.println(bip.test(2,2));
+        System.out.println(bip.test(25,5));
+
+    }
+}
